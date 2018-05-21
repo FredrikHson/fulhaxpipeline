42ba04c01e40e04b46f0bb7b5f666832b5590c45
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2018-05-21 21:28:14 +0200

    testing library for slack

---
+++ vars/slackstatus.groovy
@@ -0,0 +1,15 @@
+def call(String status = 'success')
+{
+    tokens = "${env.JOB_NAME}".tokenize('/')
+    org = tokens[tokens.size()-3]
+    repo = tokens[tokens.size()-2]
+    branch = tokens[tokens.size()-1]
+    switch( status )
+    {
+        case "success":
+        slackSend color: 'good', message: "${org} ${repo} ${branch} built successfully"
+        break
+        case "fail":
+        slackSend color: 'bad', message: "${org} ${repo} ${branch} failed to build ${env.BUILD_URL}console"
+    }
+}
