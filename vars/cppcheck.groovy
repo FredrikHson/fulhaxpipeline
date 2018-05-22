d2470016dd1bac0d93801880ee08c8aefd45cbdb
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2018-05-22 08:26:35 +0200

    oddness

--- vars/cppcheck.groovy
+++ vars/cppcheck.groovy
@@ -1,6 +1,8 @@
 def call(String folders = '.')
 {
-    sh "cppcheck --enable=all --template="{file},{line},{severity},{id},{message}" ${folders} 2> cppcheck.txt"
+    sh """
+    cppcheck --enable=all --template="{file},{line},{severity},{id},{message}" ${folders} 2> cppcheck.txt
+    """
     warnings canComputeNew: false,
     canResolveRelativePaths: false,
     categoriesPattern: '',
