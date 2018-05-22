cde3d84351d5ceccc62902bcd144bd1053accad1
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2018-05-22 08:17:49 +0200

    cppcheck

---
+++ vars/cppcheck.groovy
@@ -0,0 +1,15 @@
+def call(String folders = '.')
+{
+    sh "cppcheck --enable=all --template="{file},{line},{severity},{id},{message}" ${folders} 2> cppcheck.txt"
+    warnings canComputeNew: false,
+    canResolveRelativePaths: false,
+    categoriesPattern: '',
+    defaultEncoding: '',
+    excludePattern: '',
+    healthy: '',
+    includePattern: '',
+    messagesPattern: '',
+    parserConfigurations: [[parserName: 'cppcheck',
+    pattern: 'cppcheck.txt']],
+    unHealthy: ''
+}
