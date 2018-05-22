54fa5b617ccd7cf61ef5902ee745137549ecdd7e
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2018-05-22 08:38:03 +0200

    add the warnings to the log

--- vars/cppcheck.groovy
+++ vars/cppcheck.groovy
@@ -1,7 +1,7 @@
 def call(String folders = '.')
 {
     sh """
-    cppcheck --enable=all --template="{file},{line},{severity},{id},{message}" ${folders} 2> cppcheck.txt
+    cppcheck -q --enable=all --template="{file},{line},{severity},{id},{message}" ${folders} 2>&1 | tee cppcheck.txt
     """
     warnings canComputeNew: false,
     canResolveRelativePaths: false,
