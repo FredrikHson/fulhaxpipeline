a8bba436ebc3f9db2b28cb0abc0a8dfc7f83731d
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2018-05-22 12:01:49 +0200

    add gcc varnings function

---
+++ vars/gccwarnings.groovy
@@ -0,0 +1,15 @@
+def call()
+{
+    warnings canComputeNew: false,
+    canResolveRelativePaths: false,
+    categoriesPattern: '',
+    consoleParsers: [[parserName: 'GNU C Compiler 4 (gcc)']],
+    defaultEncoding: '',
+    excludePattern: '',
+    failedTotalHigh: '0',
+    healthy: '',
+    includePattern: '',
+    messagesPattern: '',
+    unHealthy: '',
+    unstableTotalNormal: '0'
+}
