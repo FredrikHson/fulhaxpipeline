45f4ec9d6ba6606cf638a0683497667933c9821a
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2018-07-03 10:03:06 +0200

    formating

--- vars/slackstatus.groovy
+++ vars/slackstatus.groovy
@@ -33,17 +33,17 @@ def call(String status = 'success')
     switch( status )
     {
         case "success":
-        slackSend color: 'good', message: "${org}/${repo}/${branch} built successfully. ${change}"
+        slackSend color: 'good', message: "${org}/${repo}/${branch} built successfully.\n${change}"
         break
         case "failure":
         case "fail":
-        slackSend color: '#ff0000', message: "${org}/${repo}/${branch} failed to build ${env.BUILD_URL}console ${change}"
+        slackSend color: '#ff0000', message: "${org}/${repo}/${branch} failed to build ${env.BUILD_URL}console\n${change}"
         break
         case "facepalm":
-        slackSend color: '#6c3000', message: "${org}/${repo}/${branch} facepalmed to hard ${env.BUILD_URL} ${change}"
+        slackSend color: '#6c3000', message: "${org}/${repo}/${branch} facepalmed to hard ${env.BUILD_URL}\n${change}"
         break
         case "unstable":
-        slackSend color: '#6c3000', message: "${org}/${repo}/${branch} had an earthquake ${env.BUILD_URL} ${change}"
+        slackSend color: '#6c3000', message: "${org}/${repo}/${branch} had an earthquake ${env.BUILD_URL}\n${change}"
         break
 
     }
