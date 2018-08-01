89c47127409c66adb060e121cc9dee36d1d4ed6b
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2018-08-01 09:16:43 +0200

    added a function for cppcheck using compile_commands.json

---
+++ vars/cppcheckcommands.groovy
@@ -0,0 +1,19 @@
+def call()
+{
+    sh """
+    cppcheck -q --enable=all --suppress='unusedFunction' --template="{file},{line},{severity},{id},{message}" --project=$(find -name compile_commands.json | head -n1) 2>&1 | tee cppcheck.txt
+    """
+    warnings canComputeNew: false,
+    canResolveRelativePaths: false,
+    categoriesPattern: '',
+    defaultEncoding: '',
+    excludePattern: '',
+    healthy: '',
+    includePattern: '',
+    messagesPattern: '',
+    parserConfigurations: [[parserName: 'cppcheck',
+    pattern: 'cppcheck.txt']],
+    failedTotalHigh: '0',
+    unstableTotalNormal: '0',
+    unHealthy: ''
+}
